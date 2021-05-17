622fd8ba81015d94f0fb85839e653a6432661996
VladTomici14 <vladtomici@gmail.com>
2021-05-18 00:18:36 +0300

    added faceDetector.py file.

---
+++ code/faceDetector.py
@@ -0,0 +1,30 @@
+import cv2
+import argparse
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+import time
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required = False)
+ap.add_argument("-p", "--path", required = False)
+args = vars(ap.parse_args())
+
+faceCascadePath = args["path"]
+
+camera = PiCamera()
+
+if __name__ == "__main__":
+    camera.start_preview()
+    while True:
+        T, frame = camera.read()
+
+        auxFrame = frame.copy()
+        cv2.imshow("camera", auxFrame)
+
+        if cv2.waitKey(1) and 0xFF == ord("q"):
+            break
+    camera.stop_preview()
+
+    camera.release()
+    cv2.destroyAllWindows()
+    cv2.waitKey(0)

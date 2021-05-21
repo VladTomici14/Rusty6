7d6052ad31ea8d4176c96735c4536310763b8d0d
VladTomici14 <vladtomici@gmail.com>
2021-05-22 00:55:37 +0300

    edited the relay.py file

--- code/relay.py
+++ code/relay.py
@@ -1,31 +1,8 @@
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
-motor_relay = 12
-solar_relay = 23
-
-GPIO.setup(motor_relay, GPIO.OUT)
-GPIO.output(motor_relay, GPIO.LOW)
-
-time.sleep(2)
-
-GPIO.output(motor_relay, GPIO.HIGH)
-time.sleep(4)
-GPIO.output(motor_relay, GPIO.LOW)
-
-"""if __name__ == "__main__":
-    while True:
-        command = str(input("enter: "))
-        k = 0
-        if command == "k":
-            k = k + 1
-        elif command == "q":
-            break
-
-        if k % 2 == False:
-            GPIO.output(motor_relay, GPIO.LOW)
-        elif k % 2 == True:
-            GPIO.output(motor_relay, GPIO.HIGH)"""
+relay = 12
+GPIO.setup(relay, GPIO.OUT)
+GPIO.output(relay, GPIO.LOW)
+GPIO.output(relay, GPIO.HIGH)

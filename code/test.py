759dfeb282a36f9a8eebff0395451d4b6e8d32ad
VladTomici14 <vladtomici@gmail.com>
2021-05-22 13:25:16 +0300

    edited test.py

--- code/test.py
+++ code/test.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
 
@@ -6,3 +7,5 @@ RELAIS_1_GPIO = 25
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
+time.sleep(2)
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out

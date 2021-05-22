db434e8a1df05f7ad3bf9aefa390f15d58d6f36d
VladTomici14 <vladtomici@gmail.com>
2021-05-22 13:25:45 +0300

    edited test.py

--- code/test.py
+++ code/test.py
@@ -7,5 +7,4 @@ RELAIS_1_GPIO = 25
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
-time.sleep(2)
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out

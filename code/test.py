8f73be53c916105f28929fe19be6aa14a61011f4
VladTomici14 <vladtomici@gmail.com>
2021-05-22 13:31:38 +0300

    edited test.py

--- code/test.py
+++ code/test.py
@@ -6,7 +6,9 @@ GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
 RELAIS_1_GPIO = 25
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
+time.sleep(1)
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
+time.sleep(1)
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
 
 GPIO.cleanup()

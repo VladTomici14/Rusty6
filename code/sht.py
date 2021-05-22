c600b5cf0f6fac7ee8245e13baaac2a13986330c
VladTomici14 <vladtomici@gmail.com>
2021-05-22 21:18:41 +0300

    edited the sht.py file

--- code/sht.py
+++ code/sht.py
@@ -2,12 +2,17 @@ import time
 from pi_sht1x import SHT1x as sht
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
+
 dat = 2
 sck = 3
 
-with sht(sck, dat, gpio_mode = GPIO.BCM) as sensor:
+with sht(dat, sck, gpio_mode = GPIO.BCM) as sensor:
     temperature = sensor.read_temperature()
     humidity = sensor.read_humidity(temperature)
-    print(sensor)
+
+print("temperature: {}".format(temperature))
+print("humidity: {}".format(humidity))
+
 
 

7af5a212efe242b9ab16050aa8377fa85d699370
VladTomici14 <vladtomici@gmail.com>
2021-05-22 11:10:07 +0300

    edited the relay.py file

--- code/relay.py
+++ code/relay.py
@@ -8,17 +8,25 @@ GPIO.setup(relay, GPIO.OUT)
 GPIO.output(relay, False)
 
 k = 0
-while True:
-    command = str(input("command: "))
-    if command == "k":
-        k = k + 1
-    elif command == "q":
-        break
 
-    if k % 2 == False:
-        GPIO.output(relay, False)
-    else:
-       GPIO.output(relay, True)
+try:
+    while True:
+        command = str(input("command: "))
+        if command == "k":
+            k = k + 1
+        elif command == "q":
+            break
+
+        if k % 2 == False:
+            GPIO.output(relay, False)
+        else:
+           GPIO.output(relay, True)
+
+except KeyboardInterrupt:
+    print("keyboard interrupt")
+
+except:
+    print("some error")
 
 finally:
     GPIO.cleanup()

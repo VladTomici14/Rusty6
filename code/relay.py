3e23a2d9629db34ecf71ff588c0ccbb2e550ef27
VladTomici14 <vladtomici@gmail.com>
2021-05-22 11:27:21 +0300

    edited the relay.py file

--- code/relay.py
+++ code/relay.py
@@ -4,12 +4,12 @@ import time
 GPIO.setwarnings(False)
 
 def relayOn(relay):
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(relay, GPIO.OUT)
 
 
 def relayOff():
     GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
 
 GPIO.setmode(GPIO.BCM)
 motors = 25
@@ -21,16 +21,20 @@ if __name__ == "__main__":
             command = str(input(""))
             if command == "motors on":
                 relayOff()
+                time.sleep(1)
                 relayOn(motors)
 
             elif command == "motors off":
                 relayOff()
+                time.sleep(1)
 
             elif command == "solar on":
                 relayOff()
+                time.sleep(1)
                 relayOn(solar)
 
             elif command == "solar off":
+                time.sleep(1)
                 relayOff()
 
     except KeyboardInterrupt:

e38f87cdab44f9d39098da8f2c91bd6bbab70d57
VladTomici14 <vladtomici@gmail.com>
2021-05-22 13:09:12 +0300

    edited test.py file

--- code/test.py
+++ code/test.py
@@ -1,38 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-#GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
 motors = 25
 solar = 8
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(motors, GPIO.OUT)
+GPIO.setup(solar, GPIO.OUT)
+
 try:
     while True:
-        command = str(input(""))
-        if command == "motorson":
-            GPIO.output(solar, GPIO.LOW)
-            time.sleep(0.3)
-            GPIO.output(motors, GPIO.HIGH)
+        for x in range(5):
+            GPIO.output(motors, True)
+            time.sleep(0.1)
+            GPIO.output(motors, False)
+            GPIO.output(solar, True)
+            time.sleep(0.1)
+            GPIO.output(solar, False)
 
-        elif command == "motorsoff":
-            GPIO.output(motors, GPIO.LOW)
+        GPIO.output(motors,True)
+        GPIO.output(solar,True)
 
-        elif command == "solaron":
-            GPIO.output(motors, GPIO.LOW)
-            time.sleep(3)
-            GPIO.output(solar, GPIO.HIGH)
-
-        elif command == "solaroff":
-            GPIO.output(solar, GPIO.LOW)
-
-        elif command == "exit":
-            break
-            
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-time.sleep(2)
-print("something")
-
-GPIO.cleanup()

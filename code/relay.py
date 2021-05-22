4249f065840c583eeb72cd6d2b32aca1b6bf0bef
VladTomici14 <vladtomici@gmail.com>
2021-05-22 11:51:48 +0300

    edited the relay.py file

--- code/relay.py
+++ code/relay.py
@@ -1,39 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIO.setwarnings(False)
-
-def relayOn(relay):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(relay, GPIO.OUT)
-    GPIO.output(relay, GPIO.HIGH)
-
-def relayOff():
-    GPIO.cleanup()
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+
 motors = 25
 solar = 8
 
-if __name__ == "__main__":
-    try:
-        while True:
-            command = str(input(""))
-            if command == "motors on":
-                relayOff()
-                relayOn(motors)
+try:
+    while True:
+        command = str(input(""))
+        if command == "motorson":
+            GPIO.cleanup()
+            time.sleep(0.5)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(motors, GPIO.OUT)
 
-            elif command == "motors off":
-                relayOff()
+        elif command == "motorsoff":
+            GPIO.cleanup()
 
-            elif command == "solar on":
-                relayOff()
-                relayOn(solar)
+        elif command == "solaron":
+            GPIO.cleanup()
+            time.sleep(0.5)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(solar, GPIO.OUT)
 
-            elif command == "solar off":
-                relayOff()
+        elif command == "solaroff":
+            GPIO.cleanup()
 
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-    finally:
-        GPIO.cleanup()
+        elif command == "exit":
+            break
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+GPIO.cleanup()

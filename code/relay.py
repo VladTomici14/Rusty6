db28634f86a1760e8ee4f44c8269620e7e8f9475
VladTomici14 <vladtomici@gmail.com>
2021-05-22 11:01:41 +0300

    edited the relay.py file

--- code/relay.py
+++ code/relay.py
@@ -1,15 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-relay = 25
+relay = 22
 GPIO.setup(relay, GPIO.OUT)
-GPIO.output(relay, GPIO.LOW)
-time.sleep(2)
-GPIO.output(relay, GPIO.HIGH)
-time.sleep(2)
-GPIO.output(relay, GPIO.LOW)
 
-GPIO.setwarnings(False)
+GPIO.output(relay, False)
+
+time.sleep(2)
+
+GPIO.output(relay, True)
+
+time.sleep(2)
+
+GPIO.output(relay, False)
+

242ddf3edc5022344a4a4c87bbaaa03e32405345
VladTomici14 <vladtomici@gmail.com>
2021-05-22 11:50:09 +0300

    edited the test.py file

--- code/test.py
+++ code/test.py
@@ -9,11 +9,11 @@ solar = 8
 
 try:
     while True:
-        GPIO.setmode(GPIO.BCM)
         command = str(input(""))
         if command == "motorson":
             GPIO.cleanup()
             time.sleep(0.5)
+            GPIO.setmode(GPIO.BCM)
             GPIO.setup(motors, GPIO.OUT)
 
         elif command == "motorsoff":
@@ -22,6 +22,7 @@ try:
         elif command == "solaron":
             GPIO.cleanup()
             time.sleep(0.5)
+            GPIO.setmode(GPIO.BCM)
             GPIO.setup(solar, GPIO.OUT)
 
         elif command == "solaroff":
